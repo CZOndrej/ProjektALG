d1929b149a0446857d1573dd43b819b8ef45b354
CZOndrej <on.krejci@gmail.com>
2021-05-11 09:53:31 +0200

    Comments

--- ProjektALG/Node.cpp
+++ ProjektALG/Node.cpp
@@ -1,4 +1,4 @@
-#include "Node.h"
+﻿#include "Node.h"
 
 Node::Node(int data)
 {
@@ -6,27 +6,37 @@ Node::Node(int data)
 	this->left = nullptr;
 	this->right = nullptr;
 }
-
+/**
+* Vytvoří nový ukazatel na levý uzel
+*/
 void Node::SetLeft(int data)
 {
 	this->left = new Node(data);
 }
-
+/**
+* Vytvoří nový ukazatel na pravý uzel
+*/
 void Node::SetRight(int data)
 {
 	this->right = new Node(data);
 }
-
+/**
+* Vrací ukazatel na pravý uzel
+*/
 Node* Node::GetRight()
 {
 	return this->right;
 }
-
+/**
+* Vrací ukazatel na levý uzel
+*/
 Node* Node::GetLeft()
 {
 	return this->left;
 }
-
+/**
+* Vrací číselnou hodnotu uzlu
+*/
 int Node::GetData()
 {
 	return this->data;
--- ProjektALG/ProjektALG.cpp
+++ ProjektALG/ProjektALG.cpp
@@ -1,4 +1,4 @@
-
+﻿
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -6,7 +6,9 @@
 
 using namespace std;
 
-
+/**
+* Načte soubor z předané cesty a navrací instanci vstupního datového proudu ifstream
+*/
 ifstream LoadFile(string path)
 {
     ifstream fileStream;
@@ -20,7 +22,9 @@ ifstream LoadFile(string path)
 
     return fileStream;
 }
-
+/**
+* Zařadí nový uzel na příslušné místo ve stromu za předaný výchozí uzel. 
+*/
 void AddNode(Node* node, int num)
 {
     if (num > node->GetData())
@@ -46,7 +50,9 @@ void AddNode(Node* node, int num)
         }
     }
 }
-
+/**
+* Vytvoří Binární strom z předaného datového proudu
+*/
 Node* CreateTree(ifstream stream)
 {
     Node* root = nullptr;
@@ -65,24 +71,10 @@ Node* CreateTree(ifstream stream)
     }
     return root;
 }
-
-int GetSum(Node* root)
-{
-    if (root->GetLeft() != nullptr)
-    {
-        return GetSum(root->GetLeft()) + root->GetData();
-    }
-    if (root->GetRight() != nullptr)
-    {
-        return GetSum(root->GetRight()) + root->GetData();
-    }
-    if ((root->GetRight() == nullptr && root->GetLeft() == nullptr))
-    {
-        return root->GetData();
-    }
-}
-
-int GetHeigth(Node* node)
+/**
+* Jako prametr přijímá kořenový uzel stromu a vrací počet pater binárního stromu
+*/
+int GetHeight(Node* node)
 {
     if (node == nullptr)
     {
@@ -90,8 +82,8 @@ int GetHeigth(Node* node)
     }
     else 
     {
-        int leftHeight = GetHeigth(node->GetLeft());
-        int rightHeight = GetHeigth(node->GetRight());
+        int leftHeight = GetHeight(node->GetLeft());
+        int rightHeight = GetHeight(node->GetRight());
 
         if (leftHeight > rightHeight)
         {
@@ -103,7 +95,9 @@ int GetHeigth(Node* node)
         }
     }
 }
-
+/**
+* Navrací šířku patra stromu, jako parametr přebírá kořenový uzel a číslo příslušného patra
+*/
 int GetLevelWidth(Node* root, int level)
 {
     if (root == nullptr)
@@ -119,11 +113,13 @@ int GetLevelWidth(Node* root, int level)
         return GetLevelWidth(root->GetLeft(), level - 1) + GetLevelWidth(root->GetRight(), level - 1);
     }
 }
-
+/**
+* Navrací šířku binárního stromu
+*/
 int GetMaxWidth(Node* root)
 {
     int maxWidth = 0;
-    for (int i = 1; i <= GetHeigth(root); i++)
+    for (int i = 1; i <= GetHeight(root); i++)
     {
         if (maxWidth < GetLevelWidth(root, i))
         {
@@ -135,11 +131,9 @@ int GetMaxWidth(Node* root)
 
 int main()
 {
-    Node* root = CreateTree(LoadFile("Tree2.txt"));
+    Node* root = CreateTree(LoadFile("Tree1.txt"));
 
-    
-
-    cout << GetSum(root) << endl;
+    cout << GetMaxWidth(root) << endl;
 }
 
 
